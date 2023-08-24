2175f25f5f1e63ca33c1acb0b7f19ee6b778e37e
neeraj2712 <139760045+neeraj2712@users.noreply.github.com>
2023-08-24 12:05:34 +0000

    var data

---
+++ .devcontainer/basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3)  vwe are using node js , not browser
+
+console.log(3 + 3)
+
+console.log("Neeraj");  // code readablity should be high
+
+let name = "Neeraj"
+let age = 23
+
+
+/*
+number
+bigint
+string
+boolean 
+null   --- null is a representation of empty value
+undefined
+symbol => unique
+
+
+object
+*/
+
+console.log(typeof age);
+
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 373856
+let accountEmail = "neeraj63@gmail.com"
+var accountPassword = "63428"
+accountCity = "Noida"
+let accountState;
+
+//  accountId = 17263 not change
+// console.log(accountId);
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+accountEmail = "sj@sw.com"
+accountPassword="78252"
+accountCity = "Greater Noida"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
