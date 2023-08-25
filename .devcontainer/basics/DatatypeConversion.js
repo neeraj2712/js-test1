e43a5f8262b55ad0fe934e84bbe25fa391ff3a2e
neeraj2712 <139760045+neeraj2712@users.noreply.github.com>
2023-08-25 06:54:03 +0000

    conversion of Datatypes

---
+++ .devcontainer/basics/DatatypeConversion.js
@@ -0,0 +1,35 @@
+//let score = 33
+//let score = null
+//let score = undefined
+//let score = true
+//let score = "Neeraj"   // cannot change into number (typeof is NaN)
+
+
+
+// let score = "33aba"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber );
+
+// console.log(valueInNumber);
+
+let bool = 1
+
+let loggedBool = Boolean(bool)
+//  console.log(loggedBool);
+
+// 1 => true;  0 => false
+// "" => false
+// "Neeraj" => true
+
+let num = 44
+let stringNum = String(num)
+
+console.log(stringNum);
+
+console.log(typeof stringNum);
