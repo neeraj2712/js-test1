8a1be6d407ffdf2ce411850978bc30a98f9060e9
neeraj2712 <139760045+neeraj2712@users.noreply.github.com>
2023-08-25 09:38:31 +0000

    operations

--- .devcontainer/basics/DatatypeConversion.js
+++ .devcontainer/basics/DatatypeConversion.js
@@ -30,6 +30,46 @@ let loggedBool = Boolean(bool)
 let num = 44
 let stringNum = String(num)
 
-console.log(stringNum);
+// console.log(stringNum);
 
-console.log(typeof stringNum);
+// console.log(typeof stringNum);
+
+
+
+// *************************Operations*********************************
+
+let value = 4
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);  => add 4
+// console.log(2-2);  => sub 0
+// console.log(2*2);  => mul 4
+// console.log(2**2); => pow 4
+// console.log(2/2);  => 1
+// console.log(2%2);  => 0
+
+let str1 = "Hello"
+let str2 = "World"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);  //  12
+// console.log(1 + "2");  //  12
+// console.log("1" + 2 + 2);  // 122
+// console.log(1 + 2 + "2");  //  32
+
+let counter = 100
+counter++;
+console.log(counter);
+
+
+
+
+
+
+
+
+
