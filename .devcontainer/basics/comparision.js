99112b170c7aa9d04dd71227f5fe39fac95c9457
neeraj2712 <139760045+neeraj2712@users.noreply.github.com>
2023-08-25 10:26:55 +0000

    comparision

---
+++ .devcontainer/basics/comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 3);
+// console.log(2 >= 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+
+// console.log("2" > 3);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+
+//  ===  its strict check the valve ( it also check its datatype also)
+
+console.log(2 === 2);
