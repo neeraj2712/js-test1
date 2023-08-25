60fd11864c749235ec51d79e6858d39316644a03
neeraj2712 <139760045+neeraj2712@users.noreply.github.com>
2023-08-25 12:01:22 +0000

    DataType

---
+++ .devcontainer/basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive dadatypes  ( They all are call by value)  -- the changes in the code is it in copy not in memory refrence
+
+// 7 Types -- string, Number , boolean, null , undefined, symbol, bigint
+
+// Non Primitive datatypes (refrence)
+
+// Array , object, functions
+
+const score =100
+const Hscore = 12.3
+const blog = false
+
+const lable = null
+let valueIs;
+
+const id = Symbol('33')
+const anotherId = Symbol('33')
+
+console.log(id == anotherId);
+
+const bigNumber = 286894792387n
+
+
+const heroes = ["Shaktiman","doga","Krissh"];
+
+let myObj = {
+    Nmae: "Neraj",
+    age: 23
+}
+
+
+const myFunction = function (){
+    console.log("Hello World");
+
+}
+
+
+
+
